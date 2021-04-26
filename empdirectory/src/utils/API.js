97499568ba0ab2272371622dda96af3960f2aaf7
Lita Beach <litabeach123@gmail.com>
2021-04-26 11:29:17 -0500

    created table, API call

--- empdirectory/src/utils/API.js
+++ empdirectory/src/utils/API.js
@@ -1,20 +1,9 @@
 import axios from "axios";
 
-// Export an object containing methods we'll use for accessing the Dog.Ceo API
-
+//export below gets all users (getUsers)
 export default {
-  getRandomDog: function() {
-    return axios.get("https://dog.ceo/api/breeds/image/random");
-  },
-  getDogsOfBreed: function(breed) {
-    return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
-  },
-  getBaseBreedsList: function() {
-    return axios.get("https://dog.ceo/api/breeds/list");
-  },
+  getEmp: function() {
 
-  
-  getEmployee: function() {
-    return axios.get("https://randomuser.me/");
+    return axios.get("https://randomuser.me/api/?results=200&nat=us");
   }
 };
